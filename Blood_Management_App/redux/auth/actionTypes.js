0d46293873775f198cc5109485acc549165adea0
Anandhu <anandhu.prasad@nineleaps.com>
2021-01-27 11:42:28 +0530

    implement redux-thunk, logout action, async storage for tokens

--- Blood_Management_App/redux/auth/actionTypes.js
+++ Blood_Management_App/redux/auth/actionTypes.js
@@ -1,7 +1,7 @@
 export const RETRIEVE_TOKEN = 'RETRIEVE_TOKEN';
-export const LOGIN_REQ = 'LOGIN_REQ';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const REQ = 'REQ';
+export const REQ_SUCCESS = 'REQ_SUCCESS';
+export const REQ_FAILURE = 'REQ_FAILURE';
 export const REGISTER_REQ = 'REGISTER_REQ,';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS,';
 export const REGISTER_FAILURE = 'REGISTER_FAILURE,';
--- Blood_Management_App/redux/store.js
+++ Blood_Management_App/redux/store.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './RootReducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
